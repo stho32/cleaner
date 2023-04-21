ebe55894a43e04cc00c23ba6db4cb3c62ef1cc8f
stho32 <stho32github@gmail.com>
2023-04-21 16:01:28 +0200

    The MethodLengthRule now also outputs the line where I can find the method.

--- Source/cleaner/cleaner.Domain/Rules/MethodLengthRule.cs
+++ Source/cleaner/cleaner.Domain/Rules/MethodLengthRule.cs
@@ -1,45 +1,47 @@
-namespace cleaner.Domain.Rules;
-
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-
-public class MethodLengthRule : IRule
+namespace cleaner.Domain.Rules
 {
-    public string Id => "MethodLengthRule";
-    public string Name => "Method Length Rule";
-    public string ShortDescription => "Detects methods with more than 10 semicolons in their body";
-    public string LongDescription => "This rule checks if a method contains more than 10 semicolons in its body and raises a warning if it does.";
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-    public ValidationMessage?[] Validate(string filePath, string fileContent)
+    public class MethodLengthRule : IRule
     {
-        var messages = new List<ValidationMessage>();
+        public string Id => "MethodLengthRule";
+        public string Name => "Method Length Rule";
+        public string ShortDescription => "Detects methods with more than 10 semicolons in their body";
+        public string LongDescription => "This rule checks if a method contains more than 10 semicolons in its body and raises a warning if it does.";
 
-        SyntaxTree tree = CSharpSyntaxTree.ParseText(fileContent);
-        var root = tree.GetCompilationUnitRoot();
-
-        var methods = root.DescendantNodes()
-            .OfType<MethodDeclarationSyntax>();
-
-        foreach (var method in methods)
+        public ValidationMessage?[] Validate(string filePath, string fileContent)
         {
-            var semicolonCount = method.DescendantTokens()
-                .Count(token => token.IsKind(SyntaxKind.SemicolonToken));
+            var messages = new List<ValidationMessage>();
 
-            if (semicolonCount > 10)
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(fileContent);
+            var root = tree.GetCompilationUnitRoot();
+
+            var methods = root.DescendantNodes()
+                .OfType<MethodDeclarationSyntax>();
+
+            foreach (var method in methods)
             {
-                var message = new ValidationMessage(
-                    Severity.Warning,
-                    Id,
-                    Name,
-                    $"A method '{method.Identifier.Text}' in the file '{filePath}' contains {semicolonCount} semicolons, which is more than the allowed limit of 10."
-                );
-                messages.Add(message);
-            }
-        }
+                var semicolonCount = method.DescendantTokens()
+                    .Count(token => token.IsKind(SyntaxKind.SemicolonToken));
 
-        return messages.ToArray();
+                if (semicolonCount > 10)
+                {
+                    var lineNumber = tree.GetLineSpan(method.Span).StartLinePosition.Line + 1;
+                    var message = new ValidationMessage(
+                        Severity.Warning,
+                        Id,
+                        Name,
+                        $"The method '{method.Identifier.Text}':{lineNumber} in the file '{filePath}' contains {semicolonCount} semicolons, which is more than the allowed limit of 10."
+                    );
+                    messages.Add(message);
+                }
+            }
+
+            return messages.ToArray();
+        }
     }
 }
